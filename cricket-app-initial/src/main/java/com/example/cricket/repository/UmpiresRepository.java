822cb8bb57dd72f1866920c12f57e835b37e487a
sowjanya Jain <sowjanayajainnrp@gmail.com>
2021-01-29 21:32:47 +0530

    add umpire API

---
+++ cricket-app-initial/src/main/java/com/example/cricket/repository/UmpiresRepository.java
@@ -0,0 +1,9 @@
+package com.example.cricket.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.cricket.model.Umpires;
+
+public interface UmpiresRepository extends JpaRepository<Umpires,Integer>{
+
+}
