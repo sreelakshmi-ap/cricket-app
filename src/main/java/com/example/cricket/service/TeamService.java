03ff54c3c9ac24892c2987176237a1b4e18cdd9c
Sumantha M K <sumanthmuttvalli@gmail.com>
2021-02-04 21:48:24 +0530

    Get All Teams

--- src/main/java/com/example/cricket/service/TeamService.java
+++ src/main/java/com/example/cricket/service/TeamService.java
@@ -1,5 +1,7 @@
 package com.example.cricket.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,11 @@ public class TeamService {
 		team.setTournamentId(tournamentId);
 		teamRepo.save(team);
 		return new TeamResponse(team,"Team Added",HttpStatus.OK);
+	}
+	
+	public List<Team> GetAllTeam(int tournamentId)
+	{
+		return teamRepo.findAllByTournamentId(tournamentId);
 	}
 	
 	
