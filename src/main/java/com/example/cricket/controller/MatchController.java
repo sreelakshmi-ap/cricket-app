8f2c730ab55e91c01c913606fe96050b021a8823
Sowjanya Jain <sowjanayajainnrp@gmail.com>
2021-02-09 22:35:10 +0530

    Update MatchController.java

--- src/main/java/com/example/cricket/controller/MatchController.java
+++ src/main/java/com/example/cricket/controller/MatchController.java
@@ -61,10 +61,14 @@ public class MatchController {
 	}
 	
 	@PutMapping("/stopMatch")
-	public MessageResponse stopMatch(@RequestParam int match_id, String reason){
+	public MessageResponse stopMatch(@RequestParam int match_id, String reason,String end_time){
 		Matchs match=matchRepository.findByMatchId(match_id).get();
 		match.setMatch_id(match_id);
 		match.setStopped_reason(reason);
+		LocalTime endTime=LocalTime.parse(end_time);
+		match.setEnd_time(endTime);
+		match.setStatus("Abondoned");
+		
 		matchRepository.save(match);
 		return new MessageResponse("match stopped",HttpStatus.OK);
 		
