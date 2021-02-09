4d6ad51083ca6b8212c07a5ffc72a068dad801cc
Sowjanya Jain <sowjanayajainnrp@gmail.com>
2021-02-09 20:56:07 +0530

    Update MatchController.java
    s

--- src/main/java/com/example/cricket/controller/MatchController.java
+++ src/main/java/com/example/cricket/controller/MatchController.java
@@ -70,12 +70,7 @@ public class MatchController {
 		
 	}
 
-        @PostMapping("/endOfMatch/setTeamInfo")
-	public MessageResponse setTeamInfo(@RequestParam int team_1_id,int team_2_id,int match_id,String end_time) {
-	  return teamService.setTeamInfo(team_1_id, team_2_id, match_id, end_time);
-		
-		
-    }
+     
 	
 	
 }
