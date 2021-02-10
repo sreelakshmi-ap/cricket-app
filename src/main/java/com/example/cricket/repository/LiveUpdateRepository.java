d48c87116c54cffb898ee41c8524f4b693b29d3b
Harsha-ms1 <harshams126@gmail.com>
2021-02-10 18:06:23 +0530

    update

--- src/main/java/com/example/cricket/repository/LiveUpdateRepository.java
+++ src/main/java/com/example/cricket/repository/LiveUpdateRepository.java
@@ -1,11 +1,16 @@
 package com.example.cricket.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.cricket.model.LiveUpdate;
 
+import java.util.List;
+
 @Repository
 public interface LiveUpdateRepository  extends JpaRepository<LiveUpdate, Long>{
 
+    @Query(value = "select * from liveupdate where match_id=?1 and team_id=?2",nativeQuery = true)
+  public   List<LiveUpdate> findAllByMatchIdAndTeamId(int matchId, int teamId);
 }
