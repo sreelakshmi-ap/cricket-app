aa9f1a1adb439cc744429e242cac4ac81d550884
Sumantha M K <sumanthmuttvalli@gmail.com>
2021-02-15 15:46:39 +0530

    Update TeamPlayerEntity.java

--- src/main/java/com/example/cricket/model/TeamPlayerEntity.java
+++ src/main/java/com/example/cricket/model/TeamPlayerEntity.java
@@ -23,8 +23,96 @@ public class TeamPlayerEntity {
 
     @Column
     private String designation;
+    
+    @Column
+    int runs;
+    
+    @Column
+    int hundreds;
+    
+    @Column
+    int fifties;
+    
+    @Column
+    int fours;
+    
+    @Column
+    int sixes;
+    
+    @Column
+    int wickets;
+    
+    @Column
+    int five_wickets_hauls;
+    
+    
+    
+    
 
-    public TeamPlayerEntity() {
+    
+	public int getFive_wickets_hauls() {
+		return five_wickets_hauls;
+	}
+	public void setFive_wickets_hauls(int five_wickets_hauls) {
+		this.five_wickets_hauls = five_wickets_hauls;
+	}
+	
+	public TeamPlayerEntity(int teamPlayerId, int teamId, int playerId, String designation, int runs, int hundreds,
+			int fifties, int fours, int sixes, int wickets, int five_wickets_hauls) {
+		super();
+		this.teamPlayerId = teamPlayerId;
+		this.teamId = teamId;
+		this.playerId = playerId;
+		this.designation = designation;
+		this.runs = runs;
+		this.hundreds = hundreds;
+		this.fifties = fifties;
+		this.fours = fours;
+		this.sixes = sixes;
+		this.wickets = wickets;
+		this.five_wickets_hauls = five_wickets_hauls;
+	}
+	
+	public int getRuns() {
+		return runs;
+	}
+	public void setRuns(int runs) {
+		this.runs = runs;
+	}
+	public int getHundreds() {
+		return hundreds;
+	}
+	
+	public void setHundreds(int hundreds) {
+		this.hundreds = hundreds;
+	}
+	public int getFifties() {
+		return fifties;
+	}
+	public void setFifties(int fifties) {
+		this.fifties = fifties;
+	}
+	public int getFours() {
+		return fours;
+	}
+	public void setFours(int fours) {
+		this.fours = fours;
+	}
+	public int getSixes() {
+		return sixes;
+	}
+	public void setSixes(int sixes) {
+		this.sixes = sixes;
+	}
+	public int getWickets() {
+		return wickets;
+	}
+	public void setWickets(int wickets) {
+		this.wickets = wickets;
+	}
+	
+	
+	public TeamPlayerEntity() {
 
     }
     public TeamPlayerEntity(int teamId, int playerId, String designation) {
