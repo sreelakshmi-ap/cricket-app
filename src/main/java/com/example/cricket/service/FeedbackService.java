1c2cde77c1556722c1cc06670f3a36369cec425a
Sumantha M K <sumanthmuttvalli@gmail.com>
2021-02-22 11:03:19 +0530

    minor Update

--- src/main/java/com/example/cricket/service/FeedbackService.java
+++ src/main/java/com/example/cricket/service/FeedbackService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +19,7 @@ import com.example.cricket.request.feedbackRequest;
 import com.example.cricket.response.MainResponse;
 import com.example.cricket.response.MessageResponse;
 
+@Service
 public class FeedbackService {
 	@Autowired
 	FeedbackRepository feedbackRepository;
